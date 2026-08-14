02d67b1ca6ccc4c8550f164e26d3b832037b7dc7
potoole7 <paddy7wb@gmail.com>
2026-08-14 15:15:38 +0100

    Move script for finding Spanish elevation

--- src/02_app/get_spain_elev.R
+++ src/02_app/get_spain_elev.R
@@ -1,5 +1,7 @@
+# TODO Make this into stand alone script! Missing code to load areas
+
 # get raster of elevations from areas file
-get_elev_rast <- \(areas, z = 9, bins = NULL, labels = NULL) {
+get_elev_rast <- \(areas, z = 9, bins = NULL, labels = NULL, reduce = TRUE) {
   # convert to projected CRS  # areas_proj <- st_transform(areas, IrishGrid = 29902)
   # for Spain this time, not Ireland!
   areas_proj <- st_transform(areas, crs = 25830)
@@ -12,8 +14,19 @@ get_elev_rast <- \(areas, z = 9, bins = NULL, labels = NULL) {
   # Mask DEM to the exact MULTIPOLYGON footprint
   dem_masked <- terra::mask(dem, terra::vect(areas_proj))
 
+  # If specified, 8 × 8 cells become one cell: approximately 64× fewer rows
+  dem_reduced <- dem_masked
+  if (reduce) {
+    dem_reduced <- terra::aggregate(
+      dem_masked,
+      fact = 8,
+      fun = mean,
+      na.rm = TRUE
+    )
+  }
+
   # Convert raster to a data.frame for ggplot
-  df_dem <- as.data.frame(dem_masked, xy = TRUE, na.rm = TRUE)
+  df_dem <- as.data.frame(dem_reduced, xy = TRUE, na.rm = TRUE)
   names(df_dem) <- c("x", "y", "elevation")
   # filter out negative elevations
   df_dem <- filter(df_dem, elevation >= 0)
@@ -49,15 +62,15 @@ elev_df_crop <- elev_df %>%
     y <= st_bbox(areas_proj)$ymax
   )
 
-areas_proj |>
-  ggplot() +
-  geom_sf(fill = NA, colour = "black")
+# areas_proj |>
+#   ggplot() +
+#   geom_sf(fill = NA, colour = "black")
 
 p <- areas_proj |>
   ggplot() +
-  gom_sf(fill = NA, colour = "black") +
-  gem_tile(data = elev_df_crop, aes(x = x, y = y, fill = elevation)) +
-  cec_theme(nejm_pal = FALSE, legend.position = "right")
+  geom_sf(fill = NA, colour = "black") +
+  geom_tile(data = elev_df_crop, aes(x = x, y = y, fill = elevation)) +
+  cecl_theme(nejm_pal = FALSE, legend.position = "right")
 
 ggsave(plot = p, "spain_elevation.png", width = 10, height = 8)
 
@@ -67,12 +80,12 @@ elev_df_bin <- get_elev_rast(
   areas_ccaa,
   bins = c(0, 200, 600, 1000, 1500, 2000, Inf),
   labels = c(
-    "<200 m",
-    "200–600 m",
-    "600–1000 m",
-    "1000–1500 m",
-    "1500–2000 m",
-    ">2000 m"
+    "<200",
+    "200–600",
+    "600–1000",
+    "1000–1500",
+    "1500–2000",
+    ">2000"
   )
 )
 
@@ -102,20 +115,24 @@ p1 <- areas_proj |>
   # ) +
   scale_fill_manual(
     values = c(
-      "<200 m"      = "#2E8B57",
-      "200–600 m"   = "#9ACD66",
-      "600–1000 m"  = "#F7E56B",
-      "1000–1500 m" = "#F29E38",
-      "1500–2000 m" = "#C46A3A",
-      ">2000 m"     = "#7A3B1E"
+      "<200"      = "#2E8B57",
+      "200–600"   = "#9ACD66",
+      "600–1000"  = "#F7E56B",
+      "1000–1500" = "#F29E38",
+      "1500–2000" = "#C46A3A",
+      ">2000"     = "#7A3B1E"
     ),
     drop = FALSE,
-    name = "Elevation\n(m)"
+    name = "Elevation (m)"
   ) +
   NULL
+# p1
 
-ggsave(plot = p1, "spain_elevation_binned.png", width = 10, height = 8)
+# ggsave(plot = p1, "spain_elevation_binned.png", width = 10, height = 8)
+ggsave(plot = p1, "spain_elevation_binned_reduced.png", width = 10, height = 8)
 
 # save both objects
-readr::write_csv(elev_df_crop, "data/spain_elev.csv")
-readr::write_csv(elev_df_bin_crop, "data/spain_elev_binned.csv")
+# readr::write_csv(elev_df_crop, "data/spain_elev.csv")
+# readr::write_csv(elev_df_bin_crop, "data/spain_elev_binned.csv")
+readr::write_csv(elev_df_crop, "data/spain_elev_reduced.csv")
+readr::write_csv(elev_df_bin_crop, "data/spain_elev_binned_reduced.csv")
